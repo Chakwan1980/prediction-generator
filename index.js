6f1baafce1a9a293263cc29eba96c78398e60740
Rosa Flores Lopez <rosa.lopez@edu.techstarter.de>
2024-10-16 10:46:46 +0200

    Adding lambda

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const AWS = require('aws-sdk'); 
+const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-const handler = async (event) => {
-  const predictionID = `pred-${Date.now()}`;  // Corregido el uso de $ con - 
-  const { question, prediction } = event; 
+exports.handler = async (event) => {
 
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,  // Corregido el nombre de la variable
-    Item: {
-      predictionID,
-      question,
-      prediction
+    const predictionID = `pred-$(Date.now())`;
+    const { question, prediction } = event;
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        predictionID,
+        question,
+        prediction
+      }
+    };
+
+    try {
+      await dynamodb.put(params).promise();
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Prediction saved successfully", predictionID })
+      };
+    } catch (error) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ message: "Error saving prediction", error })
+        }; 
     }
-  };
-
-  try {
-    await dynamodb.put(params).promise();
-    return  {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Prediction saved successfully", predictionID })  // Agregado una coma después de statusCode
-    };
-    
-  } catch (error) {
-    return {
-      statusCode: 500, 
-      body: JSON.stringify({ message: "Error saving prediction", error })  // Corregido el mensaje de error
-    };
-  }
 };

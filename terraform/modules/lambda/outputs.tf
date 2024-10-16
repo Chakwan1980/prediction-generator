77d270a0b00316b5f6134e44682b3b93c878bee2
Rosa Flores Lopez <rosa.lopez@edu.techstarter.de>
2024-10-16 11:14:14 +0200

    Adding lambda

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,4 +1,4 @@
 output "lambda_function_arn" {
     description = "The ARN of the Lambda function"
-    value = aws_lambda_function.lambda_function.arn
+    value = aws_lambda_function.my_lambda_function.arn  // Cambia aquí
 }
